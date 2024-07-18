380c921915146063d7e8cd0ae1e07c933e152a4a
Kamil Gwóźdź <kamil-gwozdz@github.com>
2024-07-18 11:45:56 +0200

    add generation code

--- lib/aasm/vis.rb
+++ lib/aasm/vis.rb
@@ -5,6 +5,43 @@ require_relative "vis/version"
 module AASM
   module Vis
     class Error < StandardError; end
-    # Your code goes here...
+
+    def generate_markdown
+      results = []
+
+      AASM::StateMachineStore.stores.each do |klass_name, klass_store|
+        klass_store.keys.each do |column|
+          transitions = []
+          klass = klass_name.safe_constantize
+          klass.aasm(column).events.each do |event|
+            event.name
+            event.default_display_name
+
+            event.transitions.each do |transition|
+              transitions << [transition.from, transition.to]
+            end
+          end
+
+          results << <<~TXT
+            ```mermaid
+            ---
+            title: #{klass}##{column}
+            ---
+            stateDiagram-v2
+            
+              #{klass.aasm(column).states.map { |state| "#{state.name} : #{state.default_display_name}" }.join("\n") }
+            
+              #{transitions.map { |from, to| "#{from.nil? ? "[*]" : from } --> #{to}" }.join("\n") }
+            ```
+          TXT
+        end
+      end
+
+      path = File.join(Dir.pwd,'tmp', 'assm-vis.md')
+      results = results.join("\n\n")
+
+      File.write(path, results)
+      puts "File written to: #{path}"
+    end
   end
 end
